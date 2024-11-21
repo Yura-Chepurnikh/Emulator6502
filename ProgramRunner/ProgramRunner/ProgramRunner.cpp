e716a7255e031752e3e38fb1bd01a5bc32e0659c
Yura Chepurnikh <yurichepurnikh71@gmail.com>
2024-11-21 22:32:27 +0400

    Initial commit

---
+++ ProgramRunner/ProgramRunner/ProgramRunner.cpp
@@ -0,0 +1,12 @@
+﻿// ProgramRunner.cpp: определяет точку входа для приложения.
+//
+
+#include "ProgramRunner.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
